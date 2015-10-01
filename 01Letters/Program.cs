6806f5f5d31773eb59dc32ca4b585e2f1566983f
IvomirIvanov <sprelix@abv.bg>
2015-10-01 15:32:29 +0300

    programka

---
+++ 01Letters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _01Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Regex regex = new Regex(@"(.)\1+");
+            Console.WriteLine(regex.Replace(text, "$1"));
+        }
+    }
+}
